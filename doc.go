52c135c5ef03beef3fafc5fb713b827539d7e307
Aman Mangal <mangalaman93@gmail.com>
2018-12-16 11:04:52 +0530

    Add package level documentation

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package mmap provides higher level abstractions around a memory
+// mapped file. For now, package only support creating mmap with
+// backed file on disk (shared mappings). Anonymus mappings are currently
+// not supported. Please see godoc for various function references.
+//
+package mmap
