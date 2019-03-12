1da922ade498a69b467cb07fb1e4681f021288d1
Aman Mangal <aman@dgraph.io>
2019-03-13 04:20:37 +0530

    Update API to satisfy io.ReaderAt and io.WriterAt

--- immap.go
+++ immap.go
@@ -1,14 +1,17 @@
 package mmap
 
+import "io"
+
 // IMmap provides an interface to a memory mapped file
 type IMmap interface {
+	io.ReaderAt
+	io.WriterAt
+
 	Lock() error
 	Unlock() error
 	Advise(advice int) error
-	Read(dest []byte, offset int) (int, error)
-	Write(src []byte, offset int) (int, error)
-	ReadUint64(offset int) uint64
-	WriteUint64(offset int, num uint64)
+	ReadUint64At(offset int64) (uint64, error)
+	WriteUint64At(num uint64, offset int64) error
 	Flush(flags int) error
 	Unmap() error
 }
